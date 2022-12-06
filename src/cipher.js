a92c71f509a97a15a5852a8994dd1bf22f38aead
= <=>
2022-12-06 19:06:39 -0300

    creacion de vistas y funcionalidades

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,36 @@
 const cipher = {
-  // ...
-};
+  //..
+  encode (box1, shift) { 
+    let result = "", code;
+    for (let i=0; i<box1.length;i++){
+      if (box1.charCodeAt(i)>=65 && box1.charCodeAt(i)<=90){
+        code = (((box1.charCodeAt(i)-65)+shift)%26)+65;
+      }
+      else if (box1.charCodeAt(i) >= 33 && box1.charCodeAt(i) <= 66) {
+        code = (((box1.charCodeAt(i) + 33) - shift) % 33) + 33; 
+      }
+      else if (box1.charCodeAt(i)===32){
+        code = 32;
+      }
+      result+=String.fromCharCode(code);
+    }
+    return result;
+  },
 
+  decode (box2, shift2) {
+    let result2 = "", code2;
+    for (let i=0; i<box2.length;i++){
+      if(box2.charCodeAt(i)>=65 && box2.charCodeAt(i)<=90){
+        code2 = (((box2.charCodeAt(i)+65)-shift2)%26)+65;
+      }
+      else if (box2.charCodeAt(i) >= 33 && box2.charCodeAt(i) <= 66) {
+        code2 = (((box2.charCodeAt(i) + 33) + shift2) % 33) + 33; 
+      }
+      else if (box2.charCodeAt(i)===32){
+        code2 = 32;
+      }
+      result2+=String.fromCharCode(code2);
+    }
+    return result2; }};
+  
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+document.getElementById("mensaje").addEventListener("keyup", mayuscula);
+function mayuscula (){
+  const strg = document.getElementById("mensaje");
+  strg.value = strg.value.toUpperCase();
+}
+
+document.getElementById("cifrar").addEventListener("click", function () {
+  const box1 = document.getElementById("mensaje").value; 
+  const shift = parseInt(document.getElementById("desplaz").value);
+  const result =cipher.encode(box1, shift);
+  document.getElementById("respuesta").value = result;
+})
+
+document.getElementById("descifrar").addEventListener("click", function () {
+  const box2 = document.getElementById("mensaje").value;
+  const shift2= parseInt(document.getElementById("desplaz").value);
+  const result2 = cipher.decode(box2, shift2);
+  document.getElementById("respuesta").value = result2;
+});
+    
+
+//console.log(cipher)
+
+
+
