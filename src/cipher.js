ce512fce75faf92ac4bc341f4c1c4a9a8237dd62
= <=>
2022-12-11 16:36:39 -0300

    Cambios en js y README

--- src/cipher.js
+++ src/cipher.js
@@ -1,13 +1,13 @@
 const cipher = {
   //..
-  encode (box1, shift) { 
+  encode (shift, box1) { 
     let result = "", code;
+    if (typeof shift !== "number" || typeof box1 !== "string") {
+      throw TypeError();
+    }
     for (let i=0; i<box1.length;i++){
       if (box1.charCodeAt(i)>=65 && box1.charCodeAt(i)<=90){
         code = (((box1.charCodeAt(i)-65)+shift)%26)+65;
-      }
-      else if (box1.charCodeAt(i) >= 33 && box1.charCodeAt(i) <= 66) {
-        code = (((box1.charCodeAt(i) + 33) - shift) % 33) + 33; 
       }
       else if (box1.charCodeAt(i)===32){
         code = 32;
@@ -17,20 +17,22 @@ const cipher = {
     return result;
   },
 
-  decode (box2, shift2) {
-    let result2 = "", code2;
-    for (let i=0; i<box2.length;i++){
-      if(box2.charCodeAt(i)>=65 && box2.charCodeAt(i)<=90){
-        code2 = (((box2.charCodeAt(i)+65)-shift2)%26)+65;
-      }
-      else if (box2.charCodeAt(i) >= 33 && box2.charCodeAt(i) <= 66) {
-        code2 = (((box2.charCodeAt(i) + 33) + shift2) % 33) + 33; 
-      }
-      else if (box2.charCodeAt(i)===32){
-        code2 = 32;
-      }
-      result2+=String.fromCharCode(code2);
+  decode(shift2, box2) {
+    let result = "", code;
+    if (typeof shift2 !== "number" || typeof box2 !== "string") {
+      throw TypeError();
     }
-    return result2; }};
-  
+    for (let i = 0; i < box2.length; i++) {
+      if (box2.charCodeAt(i) >= 65 && box2.charCodeAt(i) <= 90) {
+        code = (((box2.charCodeAt(i) + 65) - shift2) % 26) + 65;
+      }  
+      else if (box2.charCodeAt(i) === 32) {
+        code = 32;
+      }
+      result += String.fromCharCode(code);
+    }
+    return result;
+  },
+};
+
 export default cipher;
